5a67a949788119f22e5118ff8df36f32b2afb154
Robert Brown <ammoknight@gmail.com>
2020-12-07 09:20:08 -0700

    Use reference counting to avoid cloning strings

--- src/aoc/day7.rs
+++ src/aoc/day7.rs
@@ -3,6 +3,9 @@ use parse_display::{Display as PDisplay, FromStr as PFromStr};
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::rc::Rc;
+
+type RcString = Rc<String>;
 
 #[derive(PDisplay, PFromStr, Debug)]
 #[display("{bag} bags contain {rest}.")]
@@ -21,60 +24,58 @@ struct RawOutput {
 #[derive(Clone, Debug)]
 struct Output {
     quantity: i32,
-    bag: String,
+    bag: RcString,
 }
 
 #[derive(Clone, Debug)]
 struct Rule {
-    input: String,
+    input: RcString,
     outputs: Vec<Output>,
 }
 
 pub fn solve() {
     let file = File::open("input/day7.txt").unwrap();
     let mut rules = Vec::new();
-    let mut map: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut reverse_rules: HashMap<RcString, HashSet<RcString>> = HashMap::new();
 
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
-        let input = line.parse::<RawInput>().unwrap();
+        let raw_input = line.parse::<RawInput>().unwrap();
+        let input = Rc::new(raw_input.bag);
         let mut outputs = Vec::new();
 
-        for o in input.rest.split(", ") {
-            let raw = o.parse::<RawOutput>().unwrap();
+        for o in raw_input.rest.split(", ") {
+            let raw_output = o.parse::<RawOutput>().unwrap();
 
-            if raw.quantity == "no" {
+            if raw_output.quantity == "no" {
                 continue;
             } else {
-                let quantity = raw.quantity.parse::<i32>().unwrap();
-                let bag = raw.bag;
-                let entry = map.entry(bag.clone()).or_insert(HashSet::new());
-                entry.insert(input.bag.clone());
+                let quantity = raw_output.quantity.parse::<i32>().unwrap();
+                let bag = Rc::new(raw_output.bag);
+                reverse_rules
+                    .entry(bag.clone())
+                    .or_insert(HashSet::new())
+                    .insert(input.clone());
                 let output = Output { quantity, bag };
                 outputs.push(output);
             }
         }
 
-        let rule = Rule {
-            input: input.bag,
-            outputs,
-        };
+        let rule = Rule { input, outputs };
         rules.push(rule);
     }
 
-    let mut all_entries = map.get("shiny gold").unwrap().clone();
+    let shiny_gold = Rc::new("shiny gold".to_owned());
+    let mut all_entries: HashSet<RcString> = reverse_rules.get(&shiny_gold).unwrap().clone();
     let mut current_entries = all_entries.clone();
 
     loop {
         let mut new_entries = HashSet::new();
 
-        for e in current_entries {
-            if let Some(options) = map.get(&e) {
-                for o in options {
-                    if !all_entries.contains(o) {
-                        new_entries.insert(o.clone());
-                        all_entries.insert(o.clone());
-                    }
+        for entry in current_entries {
+            if let Some(options) = reverse_rules.get(&entry) {
+                for option in options {
+                    new_entries.insert(option.clone());
                 }
             }
         }
@@ -82,6 +83,7 @@ pub fn solve() {
         if new_entries.is_empty() {
             break;
         } else {
+            all_entries.extend(new_entries.iter().cloned());
             current_entries = new_entries;
         }
     }
