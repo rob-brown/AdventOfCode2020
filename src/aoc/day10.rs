1149294d2ca5ebf11e6139b48716f64b15643d30
Robert Brown <ammoknight@gmail.com>
2020-12-10 13:09:54 -0700

    Simplify day 10

--- src/aoc/day10.rs
+++ src/aoc/day10.rs
@@ -5,22 +5,27 @@ use std::io::{BufRead, BufReader};
 pub fn solve() {
     let file = File::open("input/day10.txt").unwrap();
     let mut all_numbers = Vec::new();
+    let mut max = 0;
 
     for line in BufReader::new(file).lines() {
         let line = line.unwrap();
         let n = line.parse::<i32>().unwrap();
         all_numbers.push(n);
+        max = max.max(n);
     }
 
     all_numbers.sort();
 
+    // Add in the final device.
+    all_numbers.push(max + 3);
+
     let mut previous = 0;
     let mut ones = 0;
-    let mut threes = 1;
+    let mut threes = 0;
     let mut run = 0;
     let mut combos: usize = 1;
 
-    for &n in all_numbers.iter() {
+    for n in all_numbers {
         let difference = n - previous;
 
         match difference {
@@ -31,6 +36,7 @@ pub fn solve() {
             3 => {
                 threes += 1;
 
+                // The run of 1s ended, calculate the new combos.
                 match run {
                     2 => combos *= 2,
                     3 => combos *= 4,
@@ -44,14 +50,6 @@ pub fn solve() {
         }
 
         previous = n;
-    }
-
-    // Catch the last run.
-    match run {
-        2 => combos *= 2,
-        3 => combos *= 4,
-        4 => combos *= 7,
-        _ => (),
     }
 
     assert_eq(Day::new(10, Part::A), 2574, ones * threes);
