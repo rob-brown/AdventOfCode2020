fd5bc50a1c7ebe6e0388729785b9300ba1f35f61
Robert Brown <ammoknight@gmail.com>
2020-12-10 00:26:38 -0700

    Add day 10 part 2

--- src/aoc/day10.rs
+++ src/aoc/day10.rs
@@ -17,17 +17,43 @@ pub fn solve() {
     let mut previous = 0;
     let mut ones = 0;
     let mut threes = 1;
+    let mut run = 0;
+    let mut combos: usize = 1;
 
     for &n in all_numbers.iter() {
         let difference = n - previous;
-        previous = n;
 
         match difference {
-            1 => ones += 1,
-            3 => threes += 1,
+            1 => {
+                ones += 1;
+                run += 1;
+            }
+            3 => {
+                threes += 1;
+
+                match run {
+                    2 => combos *= 2,
+                    3 => combos *= 4,
+                    4 => combos *= 7,
+                    _ => (),
+                }
+
+                run = 0;
+            }
             _ => (),
         }
+
+        previous = n;
+    }
+
+    // Catch the last run.
+    match run {
+        2 => combos *= 2,
+        3 => combos *= 4,
+        4 => combos *= 7,
+        _ => (),
     }
 
     assert_eq(Day::new(10, Part::A), 2574, ones * threes);
+    assert_eq(Day::new(10, Part::A), 2_644_613_988_352, combos);
 }
